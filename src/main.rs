d75296b77f2cf629ee27faaf40bfea6d87584277
Adrian Sampson <adrian@radbox.org>
2018-05-30 11:05:54 -0400

    Extract some arguments

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,29 @@ fn main() {
         .about("print descriptions of music")
         .arg(clap::Arg::with_name("album")
              .short("a")
+             .long("album")
              .help("list albums instead of tracks"))
         .arg(clap::Arg::with_name("query")
              .index(1)
              .multiple(true)
              .help("query to match music"));
 
-    clap::App::new("amaranth")
+    let app = clap::App::new("amaranth")
         .version("1.0.0")
         .arg(clap::Arg::with_name("url")
              .short("u")
              .default_value("http://localhost:8337")
              .help("server URL to connect to"))
-        .subcommand(list)
-        .get_matches();
+        .setting(clap::AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(list);
+
+    let matches = app.get_matches();
+    let url = matches.value_of("url").unwrap();
+    if let Some(matches) = matches.subcommand_matches("list") {
+        let query: Vec<&str> = matches.values_of("query").unwrap().collect();
+        let album = matches.is_present("album");
+        println!("list! {} {} {}", url, query.len(), album);
+    } else {
+        assert!(false);
+    }
 }
